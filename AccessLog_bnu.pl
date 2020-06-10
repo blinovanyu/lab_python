e0a84efb6363b6c5651bac7bdb70f1518dce49b1
blinovanyu <56169900+blinovanyu@users.noreply.github.com>
2020-06-11 01:29:31 +0700

    Update AccessLog_bnu.pl

--- AccessLog_bnu.pl
+++ AccessLog_bnu.pl
@@ -2,27 +2,28 @@ use v6;
 
 my $IP_hash = {};
 my $i=0;
+my $j=0;
 
 if (my $Log = open "C:/myperl/access.log", :r) 
 {
 	say "Readind the log-file...";
 	for $Log.lines -> $l  
 		{
-			if !(my $r = ($l ~~ /(^\d ** 0^..^4\.\d  ** 0^..^4\.\d  ** 0^..^4\.\d  ** 0^..^4) \s\- \s\-/)[0])
-				{next;}
-			if $IP_hash{$r} 
-				{$IP_hash{$r} += 1;}
-				else 
-				{$IP_hash{$r} = 1}		
+			#$j+= 1;
+			my ($r) = $l.split(' - - ');
+			#say $r, $r.WHAT;
+			if ($r ~~ /(^\d ** 0^..^4\.\d  ** 0^..^4\.\d  ** 0^..^4\.\d  ** 0^..^4)/)
+				{$IP_hash{$r}++;}
+				#last if ($j == 50);
 		}
 	close $Log;
 }
 else {die "File can't be opened";}
-say "Sorting...";
+
 if keys($IP_hash).elems != 0 
 {
 	say "TOP-10 Results:";
-	my @sorted_keys = $IP_hash.keys.sort: {$IP_hash{$^b} <=> $IP_hash{$^a}}; 
+	my @sorted_keys = $IP_hash.keys.sort: {$IP_hash{$^b} <=> $IP_hash{$^a}};
 	for ^10
 	{
 		say "@sorted_keys[$i]: $IP_hash{@sorted_keys[$i]}";
